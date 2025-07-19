e8a561f627f160f6a0dd75e0aedd2b083f6c63f0
lindi.imre <lindi.imre@devoralime.com>
2025-07-19 16:27:58 +0200

    static serving

--- baby-speechify-be/src/app.module.ts
+++ baby-speechify-be/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameSocketService } from './game-socket/game-socket.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // path to Angular dist
+      exclude: ['/api*'], // optional: exclude API routes from static handling
+    }),],
   controllers: [AppController],
   providers: [AppService, GameSocketService],
 })
